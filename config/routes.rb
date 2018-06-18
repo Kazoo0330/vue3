1ff46e3113ac81452c1c3b7770799c3752b90fd8
kazoo <kazuki_saito@diveintocode.jp>
2018-06-18 16:57:47 +0900

    controller created, jsonbuilder setting in views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'tasks/index'
+  end
+
+  namespace :api do
+    get 'tasks/create'
+  end
+
+  namespace :api do
+    get 'tasks/update'
+  end
+
   root to: 'home#index'
+  namespace :api, format: 'json' do
+    resources :tasks, only: %i[index create update]
+  end
 end
